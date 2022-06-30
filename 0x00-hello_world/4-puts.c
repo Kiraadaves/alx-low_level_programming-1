408311af2ca751fd59511fe26dd85c249527dc2b
Seun0001 <oluwaseunafolabi941@gmail.com>
2022-06-30 00:35:32 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - prints out "Programming is like building a multilingual puzzle
+ *Return: 0 if exited properly, otherwise non-zero
+ **/
+
+#include <stdio.h>
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
