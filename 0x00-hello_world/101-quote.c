155d9455d7d06925f32d5754ce8d2e6df88ca59d
Seun0001 <oluwaseunafolabi941@gmail.com>
2022-06-30 01:22:17 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints out "and that piece of art is useful"
+ * -Dora Korpar, 2015-10-19", followed by a new line, to the standard error.
+ * Returns 0 in code being correct or other output in errors.
+ **/
+#include <stdio.h>
+
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 101);
+	return (1);
+}
