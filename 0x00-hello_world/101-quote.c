8206598ac3533df57a379214082fcc5311357175
Seun0001 <oluwaseunafolabi941@gmail.com>
2022-06-30 01:37:50 -0700

    NIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
-#include <unistd.h>
-
-/**
- * main - prints out "and that piece of art is useful"
- * -Dora Korpar, 2015-10-19", followed by a new line, to the standard error.
- * Returns 0 in code being correct or other output in errors.
- **/
-#include <stdio.h>
 
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 62);
-	return (1);
+       	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+
+return (1);
 }
